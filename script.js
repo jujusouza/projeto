429b1dc6ee138f06dd1bf10c1dd79c304fc1b0f1
Juliana Souza <juliana313031@gmail.com>
2023-06-23 14:54:18 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  // pegar a tag img
+  const img = document.querySelector("#profile img")
+  // substituir a imagem
+  if (html.classList.contains("light")) {
+    // se tiver ligth mode, adicionar a imagem light
+    img.set.setAttribute("src", "/.assets.avatar-ligth.png")
+  } else {
+  }
+
+  // se tiver sem light mode, adicionar a imagem normal
+  img.set.setAttribute("src", "/.assets.avatar.png")
+}
